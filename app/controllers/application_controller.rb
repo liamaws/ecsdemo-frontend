e1970b4ec18c2549f95da21335b6dbf2deb56b29
Liam Walker <liamaws@amazon.com>
2023-02-20 14:42:12 -0800

    Update text again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
       }
 
       if res.code == '200'
-        @text = res.body[17..-1]
+        @text = res.body[17..47]
       else
         @text = "no backend found"
       end
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::Base
       }
 
       if crystalres.code == '200'
-        @crystal = crystalres.body[17..-1]
+        @crystal = crystalres.body[17..47]
       else
         @crystal = "no backend found"
       end
